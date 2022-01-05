dc2de692cf9befc3874e187919ec8190afcbf6d2
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-01-05 15:07:05 -0700

    Add files via upload
    Updated code through lesson 3.4.5.

--- PyPoll.py
+++ PyPoll.py
@@ -1,27 +1,15 @@
-# Data we need to retrieve.
-# 1. Total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. Total number of votes each candidate received
-# 4. Percentage of votes each candidate won
-# 5. The winner of the election based on popular vote
-
-
+# Add our dependencies.
 import csv
 import os
+# Assign a variable to load a file from a path.
+file_to_load = "Resources/election_results.csv"
+# Assign a variable to save the file to a path.
+file_to_save = "analysis/election_analysis.txt"
 
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("DataBootcamp/Week-3-Python/ElectionAnalysis/Resources", "election_results.csv")
-
-# create filename variable to write file
-file_to_save = os.path.join("DataBootcamp/Week-3-Python/ElectionAnalysis/analysis", "election_analysis.txt")
-
-# Open the election results and read the file
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-     #to do: read and analyze data here
-     #read the file object with the reader fxn.
-     file_reader = csv.reader(election_data)
-
-     #print each row in the csv file
-     for row in file_reader:
-          print(row)
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
