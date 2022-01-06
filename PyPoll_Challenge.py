dab098ed4c5404b2d9790781d45e40bd02288236
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-01-06 16:15:51 -0700

    updating code for deliverable 2

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -98,11 +98,11 @@ with open(file_to_save, "w") as txt_file:
         # 6c: Calculate the percentage of votes for the county.
         votes_percentage2 = float(votes2) / float(total_votes) * 100
         # 6d: Print the county results to the terminal.
-        county_results = (f"{county_name}: {votes_percentage2:.1f}% ({votes2:,} votes)")
+        county_results = (f"{county_name}: {votes_percentage2:.1f}% ({votes2:,} votes)\n")
         print(county_results)
 
          # 6e: Save the county votes to a text file.
-        
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
         if (votes2 > winning_countyvotes):
@@ -110,13 +110,14 @@ with open(file_to_save, "w") as txt_file:
             winning_countyvotes = votes2
 
     # 7: Print the county with the largest turnout to the terminal.
-    print(
+    winning_county_summary = (
         f"\n-------------------------\n"
         f"Largest County Turnout: {winning_county}\n"
         f"-------------------------\n")
+    print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
